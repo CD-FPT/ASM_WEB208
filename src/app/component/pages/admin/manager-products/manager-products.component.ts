2bfdb80f83019d6dbb481479598f0b9884289d5e
linhnguyen <linhnguyen13092003@gmail.com>
2023-05-23 01:14:26 +0700

    AW-8 manager-product

--- src/app/component/pages/admin/manager-products/manager-products.component.ts
+++ src/app/component/pages/admin/manager-products/manager-products.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { IProduct } from 'src/app/component/interface/product';
+import {ServiceService } from 'src/app/component/service/service.service'
 @Component({
   selector: 'app-manager-products',
   templateUrl: './manager-products.component.html',
   styleUrls: ['./manager-products.component.css']
 })
 export class ManagerProductsComponent {
+ status : boolean = false;
+ productName = "";
+ products: IProduct[] = []
+ product! : IProduct;
 
+    constructor(private productService: ServiceService) {
+      this.productService.getProducts().subscribe(data => {
+        this.products = data
+      })
+}
+    getValue(e: any) {
+      this.productName = e.target.value;
+    }
+    changeStatus() {
+      this.status = !this.status;
+    }
+    removeItem(id: any) {
+      this.productService.deleteProduct(id).subscribe(() => {
+        this.products = this.products.filter(item => item.id !== id)
+      }, (error) => {
+        console.log(error.message)
+      })
+  
+    }
+    
 }
--- src/app/component/service/service.service.ts
+++ src/app/component/service/service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IProduct } from '../interface/product';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-
-  constructor() { }
+  
+  constructor(private http: HttpClient) { }
+  getProducts(): Observable<IProduct[]>{
+    return this.http.get<IProduct[]>('http://localhost:3000/products');
+  }
+  deleteProduct(id: number | string): Observable<IProduct> {
+    return this.http.delete<IProduct>('http://localhost:3000/products/' + id);
+  }
 }
