f907935642be025850a3c533604283e93efd6c8e
Quanghwi <huydqph24532@fpt.edu.vn>
2023-06-11 20:46:59 +0700

    phân quyền admin - member

--- src/app/component/service/auth.service.ts
+++ src/app/component/service/auth.service.ts
@@ -1,24 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IUser } from '../interface/user';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   API_URL: string = ' http://localhost:8088/api/auth'
 
   signup(user: IUser): Observable<IUser> {
     return this.http.post<IUser>(this.API_URL + '/signup', user)
   }
+  login(role: string) {
+    if (role === "admin") {
+      this.router.navigate(['/admin'])
+    } else if (role === "member") {
+      this.router.navigate(['/'])
+    }
+  }
   signin(user: any): Observable<any> {
     return this.http.post(`${this.API_URL}/signin`, user)
   }
+
   isAuthenticated(): any {
     return JSON.parse(localStorage.getItem('credential')!) || {};
   }
+
 }
